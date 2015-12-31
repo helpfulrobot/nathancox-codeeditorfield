b27b253e2af00ed1ee320923e8a731422262c429
helpfulrobot <chris@silverstripe.com>
2016-01-01 03:52:04 +1300

    Converted to PSR-2

--- code/CodeEditorField.php
+++ code/CodeEditorField.php
@@ -1,80 +1,87 @@
 <?php
 
-class CodeEditorField extends TextareaField {
+class CodeEditorField extends TextareaField
+{
 
-	private static $allowed_actions = array (
-		'iframe'
-	);
+    private static $allowed_actions = array(
+        'iframe'
+    );
 
-	/**
-	 * @var string default_mode
-	 */
-	private static $default_mode = 'html';
+    /**
+     * @var string default_mode
+     */
+    private static $default_mode = 'html';
 
-	/**
-	 * @var string default_theme
-	 */
-	private static $default_theme;
-	
-	/**
-	 * @var string mode
-	 */
-	protected $mode;
+    /**
+     * @var string default_theme
+     */
+    private static $default_theme;
+    
+    /**
+     * @var string mode
+     */
+    protected $mode;
 
-	/**
-	 * @var string theme
-	 */
-	protected $theme;
+    /**
+     * @var string theme
+     */
+    protected $theme;
 
-	/**
-	 * @var int Visible number of text lines.
-	 */
-	protected $rows = 8;
+    /**
+     * @var int Visible number of text lines.
+     */
+    protected $rows = 8;
 
-	public function getAttributes() {
-		return array_merge(
-			parent::getAttributes(),
-			array(
-				'data-mode' => $this->getMode(),
-				'data-ace-path' => $this->getAcePath(),
-				'data-theme' => $this->getTheme()
-			)
-		);
-	}
+    public function getAttributes()
+    {
+        return array_merge(
+            parent::getAttributes(),
+            array(
+                'data-mode' => $this->getMode(),
+                'data-ace-path' => $this->getAcePath(),
+                'data-theme' => $this->getTheme()
+            )
+        );
+    }
 
-	function Field($properties = array()) {
+    public function Field($properties = array())
+    {
+        $acePath = $this->getAcePath();
 
-		$acePath = $this->getAcePath();
+        Requirements::javascript($acePath . "ace.js");
+        Requirements::javascript($acePath . "mode-" . $this->getMode() . ".js");
+    //	Requirements::javascript($acePath . "worker-" . $this->getMode() . ".js");
 
-		Requirements::javascript($acePath . "ace.js");
-		Requirements::javascript($acePath . "mode-" . $this->getMode() . ".js");
-	//	Requirements::javascript($acePath . "worker-" . $this->getMode() . ".js");
+        Requirements::javascript("codeeditorfield/javascript/CodeEditorField.js");
+        Requirements::css("codeeditorfield/css/CodeEditorField.css");
+        
+        return parent::Field($properties);
+    }
 
-		Requirements::javascript("codeeditorfield/javascript/CodeEditorField.js");
-		Requirements::css("codeeditorfield/css/CodeEditorField.css");
-		
-		return parent::Field($properties);
-	}
+    public function setMode($mode)
+    {
+        $this->mode = $mode;
+        return $this;
+    }
+    
+    public function getMode()
+    {
+        return $this->mode ? $this->mode : $this->config()->get('default_mode');
+    }
 
-	function setMode($mode) {
-		$this->mode = $mode;
-		return $this;
-	}
-	
-	function getMode() {
-		return $this->mode ? $this->mode : $this->config()->get('default_mode');
-	}
+    public function setTheme($theme)
+    {
+        $this->theme = $theme;
+        return $this;
+    }
+    
+    public function getTheme()
+    {
+        return $this->theme ? $this->theme : $this->config()->get('default_theme');
+    }
 
-	function setTheme($theme) {
-		$this->theme = $theme;
-		return $this;
-	}
-	
-	function getTheme() {
-		return $this->theme ? $this->theme : $this->config()->get('default_theme');
-	}
-
-	function getAcePath() {
-		return basename(dirname(__DIR__)) . '/thirdparty/ace/src-noconflict/';
-	}
+    public function getAcePath()
+    {
+        return basename(dirname(__DIR__)) . '/thirdparty/ace/src-noconflict/';
+    }
 }
